5163dcff9483acbd6f0f709960dd6c41b3cbccd0
Jal Irani <jirani@zerofox.com>
2017-07-27 16:35:40 -0400

    Initial Commit

---
+++ Admiral's Cup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Admiral's Cup
+//
+//  Created by Jal Irani on 7/27/17.
+//  Copyright © 2017 Jal Irani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
